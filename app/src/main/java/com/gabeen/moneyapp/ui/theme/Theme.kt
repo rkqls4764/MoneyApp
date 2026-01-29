9f040a41b1be10a6468f4e9b3ec191d4b8284a32
rkqls4764 <rkqls4764@naver.com>
2026-01-29 09:53:16 +0900

    fix: 시스템 글꼴 크기에 따른 앱 글꼴 크기 변경 방지

--- app/src/main/java/com/gabeen/moneyapp/ui/theme/Theme.kt
+++ app/src/main/java/com/gabeen/moneyapp/ui/theme/Theme.kt
@@ -1,9 +1,13 @@
 package com.gabeen.moneyapp.ui.theme
 
+import android.content.res.Configuration
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
@@ -20,4 +24,26 @@ fun MoneyAppTheme(
         typography = Typography,
         content = content
     )
+}
+
+@Composable
+fun FixedFontScaleTheme(content: @Composable () -> Unit) {
+    val configuration = LocalConfiguration.current
+    val density = LocalDensity.current
+
+    val fixedConfig = Configuration(configuration).apply {
+        fontScale = 1f
+    }
+
+    val fixedDensity = Density(
+        density = density.density,
+        fontScale = 1f
+    )
+
+    CompositionLocalProvider(
+        LocalConfiguration provides fixedConfig,
+        LocalDensity provides fixedDensity
+    ) {
+        content()
+    }
 }
