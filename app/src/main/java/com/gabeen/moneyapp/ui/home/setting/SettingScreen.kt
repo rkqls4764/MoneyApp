3a850236df8d431385b1235f781ecefe60ff5e91
rkqls4764 <rkqls4764@naver.com>
2026-02-05 09:45:04 +0900

    feat: 파일 업로드 버튼 추가

--- app/src/main/java/com/gabeen/moneyapp/ui/home/setting/SettingScreen.kt
+++ app/src/main/java/com/gabeen/moneyapp/ui/home/setting/SettingScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Category
+import androidx.compose.material.icons.filled.Upload
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -46,6 +47,12 @@ fun SettingScreen(navController: NavController) {
                 icon = Icons.Default.Category,
                 text = "카테고리 관리",
                 onClick = { navController.navigate("categoryManage") }
+            )
+
+            SettingMenuButton(
+                icon = Icons.Default.Upload,
+                text = "파일 업로드",
+                onClick = {  }
             )
         }
     }
